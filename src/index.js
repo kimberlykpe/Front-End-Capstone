d5f5b4450b5b9d92fb67de175c3b227f9a27b8e3
kimberlykpe <108425536+kimberlykpe@users.noreply.github.com>
2023-06-02 15:16:16 -0400

    navigation bar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./App.css";
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
